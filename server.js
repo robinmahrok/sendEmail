2c88ddcb8e5b2b0f75abc0910fbe7b1ca6f488a2
Ishtpreet Singh <500061526@stu.upes.ac.in>
2020-08-25 16:48:27 +0530

    V 1.0

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const shorturl = require('./models/shorturl');
+
+mongoose.connect('mongodb://localhost/urlShortner', {
+    useNewUrlParser: true, useUnifiedTopology: true
+});
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', async (req, res) =>{
+const shorturls = await shorturl.find();
+res.render('index', {shorturls: shorturls});
+});
+
+app.post('/shorturl', async (req,res)=>{
+await shorturl.create({full: req.body.fullurl});
+res.redirect('/');
+})
+
+app.get('/:shortUrl', async (req, res) =>{
+const ShortUrl= await shorturl.findOne({short: req.params.shortUrl});
+if(ShortUrl == null)
+return res.sendStatus(404);
+//console.log(shortUrl);
+ShortUrl.clicks++
+ShortUrl.save();
+res.redirect(ShortUrl.full);
+
+});
+
+app.listen(process.env.PORT || 5000);
